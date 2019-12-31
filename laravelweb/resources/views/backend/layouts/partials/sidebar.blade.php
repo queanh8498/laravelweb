c0b2290ce94c10817624714ffd9f2238220273e9
nhatminh0149 <tranlenhatminh97@gmail.com>
2019-12-31 17:07:23 +0700

    change

--- laravelweb/resources/views/backend/layouts/partials/sidebar.blade.php
+++ laravelweb/resources/views/backend/layouts/partials/sidebar.blade.php
@@ -221,16 +221,7 @@
                             Logout
                         </span>
                     </a>
-                    <!-- <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form> -->
-
+                    
                 </li>  
             </ul>
         </nav>
